a9ef5c9d7dcb8baf3566e08a6ac2cc9352b615a4
aizwellenstan <aizwellenstan@gmail.com>
2020-03-15 22:08:10 +0900

    good

--- client/ui/views/home/index.tsx
+++ client/ui/views/home/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from "graphql-tag";
+import _ from "lodash";
 
+import graphqlClient from "../../../src/api/graphql";
 import Search from "../../../src/antd/search";
 
 const query = gql`
@@ -13,20 +15,27 @@ const query = gql`
     }
 `;
 
+const MIN_QUERY_LENGTH = 3;
+const THROTTLE_TIME = 500;
+
 export const Home = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const { data, loading } = useQuery(query, {
+        skip: searchQuery.length < MIN_QUERY_LENGTH,
         variables: { searchTitle: searchQuery }
     });
+
+    const handleChange = useCallback(
+        _.throttle((searchQuery :string)=> {
+            setSearchQuery(searchQuery)
+        }, THROTTLE_TIME), 
+        [setSearchQuery]
+    )
 
     return (
         <div className="main-search-container">
             <Search
-                onChange={(searchQuery :string) => {
-                    setSearchQuery(searchQuery);
-                    console.log(searchQuery)
-                    console.log(data)
-                }}
+                onChange={handleChange}
             />
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
